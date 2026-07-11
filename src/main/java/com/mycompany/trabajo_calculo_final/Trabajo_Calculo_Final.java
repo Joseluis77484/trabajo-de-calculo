20e83daa49705e43aed12e512327e43cbfc53868
Jose <joserlot23@gmail.com>
2026-07-10 22:52:42 -0500

    intro de calculo

---
+++ src/main/java/com/mycompany/trabajo_calculo_final/Trabajo_Calculo_Final.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajo_calculo_final;
+
+/**
+ *
+ * @author José Luis
+ */
+public class Trabajo_Calculo_Final {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
