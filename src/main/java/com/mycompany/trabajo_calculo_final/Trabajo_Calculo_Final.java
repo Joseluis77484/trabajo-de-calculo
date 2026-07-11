9169cb35aa552e684275692c42df9ce476e84972
Juansebastianflorescordova-Ops <juansebastianflorescordova@gmail.com>
2026-07-11 00:25:54 -0500

    borrando comentarios

--- src/main/java/com/mycompany/trabajo_calculo_final/Trabajo_Calculo_Final.java
+++ src/main/java/com/mycompany/trabajo_calculo_final/Trabajo_Calculo_Final.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.trabajo_calculo_final;
 
-/**
- *
- * @author José Luis
- */
 public class Trabajo_Calculo_Final {
 
     public static void main(String[] args) {
