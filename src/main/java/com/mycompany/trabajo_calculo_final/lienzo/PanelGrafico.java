c045b5cb2d5452dfdf56c83aa45318f1187a9454
Juansebastianflorescordova-Ops <juansebastianflorescordova@gmail.com>
2026-07-12 21:34:11 -0500

    lienzo - metodos

--- src/main/java/com/mycompany/trabajo_calculo_final/lienzo/PanelGrafico.java
+++ src/main/java/com/mycompany/trabajo_calculo_final/lienzo/PanelGrafico.java
@@ -1,10 +1,15 @@
 package com.mycompany.trabajo_calculo_final.lienzo;
 //Trabajo realizado por: Kenyi y Sebastian
 import javax.swing.*;
+
+import com.mycompany.trabajo_calculo_final.metodos.MetodoIntegracion;
+import com.mycompany.trabajo_calculo_final.metodos.Funcion;
+
 import java.awt.*;
+
 public class PanelGrafico extends JPanel {
 
-    public void setParametros(double a, double b, double c, double d) {
+    public void setParametros(Funcion f, MetodoIntegracion metodo, double c, double d) {
         // Aquí puedes almacenar los parámetros y utilizarlos en el método paintComponent
     }
 
